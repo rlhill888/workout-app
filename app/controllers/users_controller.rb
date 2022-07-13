e74ce77a84f3e513e06e97664d98d8098444a261
RodneyHill <rlhill888@gmail.com>
2022-07-12 20:28:19 -0400

    troubleshooting

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
     def create
         
         
-        user= User.create!(create_user_params)
+        user= User.new(create_user_params)
 
         bmi_calculation= (user.weight / (user.height * user.height).to_d * 703).to_d 
         bmr_calculation= 66+(13.7 * (user.weight * 0.453592))+(5*(user.height * 2.54))-(6.8 * user.age)
@@ -21,7 +21,7 @@ class UsersController < ApplicationController
         user.protein_macros= user.weight
 
         
-        user.save
+       
 
         if user.initial_form_activity_level == "Little or no exercise"
             user.calories = user.bmr * 1.2
