1568b43e32a48a373f0f11ce058af34f76c0677f
RodneyHill <rlhill888@gmail.com>
2022-08-31 17:56:50 -0400

    troubleshooting

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'example.com'
+    origins 'https://workout-4-me-application.herokuapp.com'
 
     resource '*',
       headers: :any,
